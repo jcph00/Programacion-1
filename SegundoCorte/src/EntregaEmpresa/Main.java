92b4096f19dbe5ac531514872f3951222fb363fc
jcph00 <jcpolaniah@uqvirtual.edu.co>
2026-03-19 12:27:46 -0500

    Modificacion Main Empresa

--- SegundoCorte/src/EntregaEmpresa/Main.java
+++ SegundoCorte/src/EntregaEmpresa/Main.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Empresa empresa = new Empresa("Tech Solutions");
+        System.out.println(empresa + "\n");
         
         Empleado empleado1 = new Empleado("Juan Perez", "123456789", LocalTime.of(8, 30), LocalTime.of(17, 0));
         Empleado empleado2 = new Empleado("Maria Gomez", "987654321", LocalTime.of(9, 0), LocalTime.of(18, 0));
@@ -17,7 +18,8 @@ public class Main {
         empresa.agregarEmpleado(empleado3);
         LocalTime horaEntradaEmpresa = LocalTime.of(8, 0);
         ArrayList<Empleado> empleadosTarde = empresa.consultarEmpleadosTarde(horaEntradaEmpresa);
-        System.out.println("Empleados que llegaron tarde:");
+        System.out.println("Hora de entrada de la empresa: " + horaEntradaEmpresa + "\n");
+        System.out.println("Empleados que llegaron tarde: \n");
         for (Empleado empleado : empleadosTarde) {
             System.out.println(empleado);
         }
